5719622764187e9943bddc776a444c431dd2ca60
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-02-23 15:31:24 +0300

    Правки по замечаниям

--- js/backend.js
+++ js/backend.js
@@ -3,20 +3,18 @@
 (function () {
   var URL_POST = 'https://js.dump.academy/code-and-magick';
   var URL_GET = 'https://js.dump.academy/code-and-magick/data';
-  var StatusCode = {
-    OK: 200
-  };
+  var statusOk = 200;
   var TIMEOUT_IN_MS = 10000;
 
-  window.load = function (onLoad, onError) {
+  var setup = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
+      if (xhr.status === statusOk) {
         onLoad(xhr.response);
       } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        onError(xhr.response);
       }
     });
     xhr.addEventListener('error', function () {
@@ -28,31 +26,29 @@
 
     xhr.timeout = TIMEOUT_IN_MS;
 
+    return xhr;
+  };
+
+  window.load = function (onLoad, onError) {
+    var xhr = setup(onLoad, onError);
     xhr.open('GET', URL_GET);
     xhr.send();
   };
 
   window.save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = TIMEOUT_IN_MS;
-
+    var xhr = setup(onLoad, onError);
     xhr.open('POST', URL_POST);
     xhr.send(data);
+  };
+
+  window.errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 5px auto; text-align: center; background-color: red; border: 2px solid black';
+    node.style.position = 'fixed';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
   };
 })();
--- js/wizardsGenerator.js
+++ js/wizardsGenerator.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-  // var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  // var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARDS_COUNT = 4;
 
   var setup = document.querySelector('.setup');
@@ -20,26 +18,12 @@
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
-    // wizardElement.querySelector('.setup-similar-label').textContent = getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES);
-    // wizardElement.querySelector('.wizard-coat').style.fill = getRandomElement(window.wizard.WIZARD_COAT);
-    // wizardElement.querySelector('.wizard-eyes').style.fill = getRandomElement(window.wizard.WIZARD__EYES);
-
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
     wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
-
-  // var createWizards = function () {
-  //   var fragment = document.createDocumentFragment();
-  //   for (var i = 0; i < WIZARDS_COUNT; i++) {
-  //     fragment.appendChild(renderWizard());
-  //   }
-  //   similarListElement.appendChild(fragment);
-  // };
-
-  // createWizards();
 
   var successHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
@@ -52,17 +36,5 @@
     setup.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; color: red; background-color: white; border: 2px solid red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '24px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
-  window.load(successHandler, errorHandler);
+  window.load(successHandler, window.errorHandler);
 })();
